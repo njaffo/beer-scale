2623c63cd26df06c7952514a2afa8adb3f4418ba
Baba Buehler <bbuehler@square-root.com>
2014-02-27 18:06:17 -0600

    add home controller and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BeerScale::Application.routes.draw do
+
+  root "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
