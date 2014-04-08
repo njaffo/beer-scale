30611630694f375662f244071fcaf67b153be8a7
errant01 <errant01@gmail.com>
2014-04-08 15:24:57 -0500

    fixing attr_accessible for rails 4

--- app/controllers/weight_data_feed_infos_controller.rb
+++ app/controllers/weight_data_feed_infos_controller.rb
@@ -1,4 +1,9 @@
 class WeightDataFeedInfosController < ApplicationController
   def new
   end
+
+  private
+  def app_params
+    params.require(:weight_data_feed_info).permit(:last_received_raw, :last_received_created_at, :last_stored_raw, :last_stored_created_at)
+  end
 end
--- app/models/weight_data_feed_info.rb
+++ app/models/weight_data_feed_info.rb
@@ -1,3 +1,2 @@
 class WeightDataFeedInfo < ActiveRecord::Base
-  attr_accessible :last_received_raw, :last_received_created_at, :last_stored_raw, :last_stored_created_at
 end
