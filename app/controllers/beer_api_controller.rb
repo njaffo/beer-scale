970f298dadee8689c07182c552642978b61fed67
errant01 <errant01@gmail.com>
2014-04-14 18:44:59 -0500

    fix format statement

--- app/controllers/beer_api_controller.rb
+++ app/controllers/beer_api_controller.rb
@@ -15,7 +15,7 @@ class BeerApiController < ApplicationController
     @json_keg = Keg.find(@weight_data_info.keg_id)
 
     respond_to do |format|
-      format.json { render json: 'beer_now' }  # , status: :created, location: @weight
+      format.json { render action: 'beer_now' }  # , status: :created, location: @weight
       format.html { head :no_content }
     end
   end
