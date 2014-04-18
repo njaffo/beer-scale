6900834381c1c04e250153ba54554585c11d3167
Baba Buehler <bbuehler@square-root.com>
2014-04-18 18:59:58 -0500

    use central time and time formatting

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+BEERTIME_FORMAT = "%B %-d, %l:%M%P"
+Time::DATE_FORMATS[:beer] = BEERTIME_FORMAT
+
 # Initialize the Rails application.
 BeerScale::Application.initialize!
