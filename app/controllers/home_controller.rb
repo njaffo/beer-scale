7cb1fd20f2b54cb515aa446820ccaebe31596b95
errant01 <errant01@gmail.com>
2014-04-30 10:34:50 -0500

    Fixing n+1 query problem on weights queries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,13 @@ class HomeController < ApplicationController
   def index
     # @weights = Weight.all
     # @weights = Weight.where("created_at > '2014-04-09'")
-    @weights = Weight.where("keg_id = ?", @weight_data_info.keg_id).order(:created_at)
+    @weights = Weight.includes(:keg).where("keg_id = ?", @weight_data_info.keg_id).order(:created_at)
     @keg = Keg.find(@weight_data_info.keg_id)
     if(!@keg.end_date)
       @events_by_keg = Event.where("day > ?", @keg.start_date)
     else
       @events_by_keg = Event.where("day > ? AND day < ?", @keg.start_date, @keg.end_date)
     end
-    logger.info("  ----  events_by_keg: "+ @events_by_keg.to_s)
   end
 
   private
